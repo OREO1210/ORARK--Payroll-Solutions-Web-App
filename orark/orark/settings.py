467a0f9775cd5af2b268a10e153a8c0d6760ccf6
Dark <soumyadwip@outlook.in>
2021-10-18 21:26:59 +0530

    Renaming done

--- orark/orark/settings.py
+++ orark/orark/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'accounts',
+    'empapp',
+    'hodapp',
 ]
 
 MIDDLEWARE = [
