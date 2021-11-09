4c5d3355857c7991830424e10cd90f876fd45573
OREO1210 <bittoo979@gmail.com>
2021-11-09 20:32:28 +0530

    c

--- orark/salarymanagement/views.py
+++ orark/salarymanagement/views.py
@@ -18,8 +18,8 @@ def view_login(request):
             user = authenticate(email=email, password=password)
             if user is not None and user.is_superuser:
                     login(request,user)
-                    currentMonth = datetime.now().month
-                    currentYear = datetime.now().year
+                    currentMonth = datetime.datetime.now().month
+                    currentYear = datetime.datetime.now().year
                     return render(request,"saldet.html",{'m':currentMonth,'y':currentYear})
             else:
                 messages.error(request,"Invalid email or password")
